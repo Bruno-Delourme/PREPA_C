aa0d80d6a70f227de45adec8f562005b69b1e849
root <root@DESKTOP-N6NT6OR>
2024-09-13 17:17:36 +0200

    ft_rev_int_tab

--- sandbox.c
+++ sandbox.c
@@ -1,21 +1,27 @@
-int	ft_strlen(char *str)
+/*Écrire une fonction void ft_rev_int_tab(int *tab, int size); qui inverse les éléments 
+ d'un tableau d'entiers.
+
+tab : le tableau d'entiers à inverser.
+size : la taille du tableau. */
+
+void ft_rev_int_tab(int *tab, int size)
 {
+	int 	temp_tab;
 	int	i;
-	
+
 	i = 0;
-	while (str[i])
+	(while i < (size / 2))
 	{
-		i++;
+		temp_tab = tab[i]
 	}
-	return (i);
 }
 
 #include <stdio.h>
 int	main()
 {
 
-	char str[]="kjasdhajksdhasdjkhaksjdh";
+	char str[]="1, 2, 3, 4, 5";
 	
-	printf("%d \n", ft_strlen(str));
+	printf("%d \n", ft_rev_int_tab(str));
 	return (0);
 }
